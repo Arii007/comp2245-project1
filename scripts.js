502c2019ca85d1cce55eb390ba5d17d5d7dc7d0b
Arii07 <williamsariel68@gmail.com>
2024-11-06 18:50:58 -0400

    Finalized Project 1 layout and styles

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-/* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const form = document.querySelector('.newsletter form');
+    const emailInput = document.querySelector('.newsletter input[type="email"]');
+    const messageDiv = document.querySelector('.newsletter .message');
+
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        
+        if (emailInput.value) {
+            
+            messageDiv.textContent = `Thank you! Your email address ${emailInput.value} has been added to our mailing list!`;
+            messageDiv.style.color = "green"; 
+        } else {
+            
+            messageDiv.textContent = "Please enter a valid email address.";
+            messageDiv.style.color = "red"; 
+        }
+
+       
+        emailInput.value = '';
+    });
+});
